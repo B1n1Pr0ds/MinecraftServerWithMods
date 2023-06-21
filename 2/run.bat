d1879c35da43ac7cc71ee97137e149ff1cdd0492
B1n1Pr0ds <123495359+B1n1Pr0ds@users.noreply.github.com>
2023-06-21 16:41:15 -0300

    1

--- 2/run.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Java\jre-1.8\bin/java.exe" -Xmx6112M -Xms6112M -jar forge.jar nogui
-pause
--- Server com mods/Run.bat
+++ Server com mods/Run.bat
@@ -1,3 +1,2 @@
-@echo off
-java -Xmx6112M -Xmx6112M -jar ServerMods.jar nogui
+"C:\Program Files\Java\jre-1.8\bin\java.exe" -Xmx4096M -Xms4096M -jar forge.jar nogui
 pause
